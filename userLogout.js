c11348bb383c8797426bae27410f2679cbae3b56
rifatsust51 <61629666+rifatsust51@users.noreply.github.com>
2022-07-29 01:21:15 +0800

    Create userLogout.js

---
+++ userLogout.js
@@ -0,0 +1,17 @@
+
+
+
+
+import LogoutRoute from '../component/logoutRoute';
+import NavbarAfterLogin from '../layout/navbarAfterLogin';
+
+function UserLogOut() {
+  return (
+    <div>
+      <NavbarAfterLogin />
+      <LogoutRoute />
+    </div>
+  )
+}
+
+export default UserLogOut;
