e84992a26336784dcc15d35b50770d3f3c7a5e17
rifatsust51 <61629666+rifatsust51@users.noreply.github.com>
2022-07-29 01:10:12 +0800

    Create error.js

---
+++ error.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+import Navbar from '../layout/navbar';
+import NavbarAfterLogin from '../layout/navbarAfterLogin';
+ function Error() {
+  let isLoggedIn = localStorage.getItem("isLoggedIn");
+  return (
+    <div>
+        {isLoggedIn? <NavbarAfterLogin /> : <Navbar />}
+        <h3>Error! Page not found</h3>
+    </div>
+  )
+}
+export default Error;
